fc06fc7793a6e8dbf2e2e25898dfc2472a537bdc
rzy11034@163.com <rzy11034@163.com>
2021-02-18 16:56:22 +0800

    2021-02-18 16:56

--- Typhon/Source/Chapter12-Shortest-Path/GTA.Dijkstra.pas
+++ Typhon/Source/Chapter12-Shortest-Path/GTA.Dijkstra.pas
@@ -15,6 +15,7 @@ type
   private
     _Dis: TArr_int;
     _Graph: TWeightedGraph;
+    _Visited: TArr_bool;
 
   public
     constructor Create(g: IWeightedGraph; s: integer);
@@ -32,7 +33,12 @@ begin
   SetLength(_Dis, _Graph.Vertex);
   TArrayUtils_int.FillArray(_Dis, Integer.MaxValue);
   _Dis[s] := 0;
+  SetLength(_Visited, _Graph.Vertex);
 
+  while True do
+  begin
+
+  end;
 
 end;
 
