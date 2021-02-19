64f0f5eb8bc3d044fde232634308080f7ae875da
rzy11034 <rzy11034@163.com>
2021-02-20 00:56:46 +0800

    2021-02-20 01:00

--- Typhon/Source/Chapter12-Shortest-Path/GTA.Leetcode_743_Dijkstra.pas
+++ Typhon/Source/Chapter12-Shortest-Path/GTA.Leetcode_743_Dijkstra.pas
@@ -1,12 +1,15 @@
 unit GTA.Leetcode_743_Dijkstra;
 
 {$mode objfpc}{$H+}
+{$ModeSwitch advancedrecords}
 
 interface
 
 uses
   Classes,
   SysUtils,
+  Math,
+  DeepStar.DSA.Tree.PriorityQueue,
   DeepStar.Utils;
 
 type
@@ -45,9 +48,26 @@ type
       constructor Create(n: integer);
       destructor Destroy; override;
       procedure AddEdge(v, w, weight: integer);
+      function Adj(v: integer): TArr_int;
+      function GetWeight(v, w: integer): integer;
 
       property Vertex: integer read _Vertex;
     end;
+
+    TPair = record
+      Vertex: integer;
+      Weight: integer;
+      class function Create(newVertex, newWeight: integer): TPair; static;
+      class function Comparer(constref a, b: TPair): integer; static;
+    end;
+
+    TQueue_TPair = specialize TPriorityQueue<TPair>;
+
+  private
+    _Dis: TArr_int;
+    _Visited: TArr_bool;
+
+    procedure __Dijkstra(g: TWeightGraph; k: integer);
 
   public
     function NetworkDelayTime(times: TArr2D_int; n, k: integer): integer;
@@ -60,23 +80,70 @@ implementation
 
 procedure Main;
 begin
+  //示例 1：
+  //输入：times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+  //输出：2
+  //with TSolution.Create do
+  //begin
+  //  WriteLn(NetworkDelayTime([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2));
+  //  Free;
+  //end;
 
+  //示例 2：
+  //输入：times = [[1,2,1]], n = 2, k = 1
+  //输出：1
+  //with TSolution.Create do
+  //begin
+  //  WriteLn(NetworkDelayTime([[1, 2, 1]], 2, 1));
+  //  Free;
+  //end;
+
+  //示例 3：
+  //输入：times = [[1,2,1]], n = 2, k = 2
+  //输出：-1
+  with TSolution.Create do
+  begin
+    WriteLn(NetworkDelayTime([[1, 2, 1]], 2, 2));
+    Free;
+  end;
+end;
+
+{ TSolution.TPair }
+
+class function TSolution.TPair.Comparer(constref a, b: TPair): integer;
+begin
+  Result := a.Weight - b.Weight;
+end;
+
+class function TSolution.TPair.Create(newVertex, newWeight: integer): TPair;
+var
+  res: TPair;
+begin
+  res.Vertex := newVertex;
+  res.Weight := newWeight;
+  Result := res;
 end;
 
 { TSolution.TWeightGraph }
 
 constructor TSolution.TWeightGraph.Create(n: integer);
 var
-  i: Integer;
+  i: integer;
 begin
   SetLength(_Adj, n);
-  for i := 0 to high(_Adj) do
+  for i := 0 to High(_Adj) do
     _Adj[i] := TTreeMap_int_int.Create;
 end;
 
 procedure TSolution.TWeightGraph.AddEdge(v, w, weight: integer);
 begin
   _Adj[v].Add(w, weight);
+  _Adj[w].Add(v, weight);
+end;
+
+function TSolution.TWeightGraph.Adj(v: integer): TArr_int;
+begin
+  Result := _Adj[v].Keys;
 end;
 
 destructor TSolution.TWeightGraph.Destroy;
@@ -89,11 +156,61 @@ begin
   inherited Destroy;
 end;
 
+function TSolution.TWeightGraph.GetWeight(v, w: integer): integer;
+begin
+  Result := _Adj[v][w];
+end;
+
 { TSolution }
 
 function TSolution.NetworkDelayTime(times: TArr2D_int; n, k: integer): integer;
+var
+  g: TWeightGraph;
+  res, i: integer;
 begin
+  TArrayUtils_int.SetLengthAndFill(_Dis, n, integer.MaxValue);
+  TArrayUtils_bool.SetLengthAndFill(_Visited, n, false);
 
+  g := TWeightGraph.Create(n);
+  try
+    for i := 0 to High(times) do
+      g.AddEdge(times[i, 0] - 1, times[i, 1] - 1, times[i, 2]);
+
+    __Dijkstra(g, k - 1);
+    res := Math.MaxIntValue(_Dis);
+    Result := IfThen(res = integer.MaxValue, -1, res);
+  finally
+    g.Free;
+  end;
+end;
+
+procedure TSolution.__Dijkstra(g: TWeightGraph; k: integer);
+var
+  queue: TQueue_TPair;
+  v, w: integer;
+begin
+  _Dis[k] := 0;
+  queue := TQueue_TPair.Create(TQueue_TPair.TCmp.Construct(@TPair.Comparer));
+  try
+    queue.EnQueue(TPair.Create(k, 0));
+
+    while not queue.IsEmpty do
+    begin
+      v := TPair(queue.DeQueue).Vertex;
+      _Visited[v] := true;
+
+      for w in g.Adj(v) do
+      begin
+        if not _Visited[w] then
+        begin
+          _Dis[w] := _Dis[v] + g.GetWeight(v, w);
+          queue.EnQueue(TPair.Create(w, _Dis[w]));
+        end;
+      end;
+    end;
+  finally
+    queue.Free;
+  end;
 end;
 
 end.
--- Typhon/Source/GTA.Main.pas
+++ Typhon/Source/GTA.Main.pas
@@ -16,18 +16,11 @@ procedure Run;
 implementation
 
 uses
-  GTA.Dijkstra_More_about_Dijkstra;
+  GTA.Leetcode_743_Dijkstra;
 
 procedure Test;
-var
-  t: TTreeMap_int_int;
 begin
-  t := TTreeMap_int_int.Create;
-  t.Add(1, 1);
-  t.Add(1, 2);
 
-  TArrayUtils_int.Print(t.Keys);
-  TArrayUtils_int.Print(t.Values);
 end;
 
 procedure Run;
